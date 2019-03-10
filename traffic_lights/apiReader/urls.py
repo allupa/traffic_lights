7f41714737b779d83629c1e676008d4a3deef511
Aleksanteri <aleksanteri.saarela@hotmail.com>
2019-03-10 14:47:46 +0200

    Signup, login and profile

--- traffic_lights/apiReader/urls.py
+++ traffic_lights/apiReader/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     #path('', views.comment, name='comment'),
+    path('signup/', views.signup, name='signup'),
+    path('home/', views.home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html')),
+    path('accounts/profile/', views.profile, name='profile'),
     ]
--- traffic_lights/apiReader/views.py
+++ traffic_lights/apiReader/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 import json
 from django.http import HttpResponse
 from django.http import JsonResponse
 from apiReader.models import *
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 #from .models import trafficDetector
@@ -21,3 +24,25 @@ from apiReader.models import *
 
 #    text_list = Comment.objects.all()
  #   return render(request, "comment.html", {'text_list':text_list})
+
+def home(request):
+    return render(request, 'extensions/home.html')
+
+def profile(request):
+    return render(request, 'extensions/profile.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request,user)
+            return redirect('/accounts/profile')
+    else:
+        form = UserCreationForm()
+    return render(request, 'extensions/signup.html', {'form': form})
+
